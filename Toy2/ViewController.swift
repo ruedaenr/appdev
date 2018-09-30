d1b78ebbb1bb3f7b15f62aa25cbc147329fc04e1
Enrique Rueda <ruedaenr@grinnell.edu>
2018-09-29 20:17:36 -0500

    Initial Commit

---
+++ Toy2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Toy2
+//
+//  Created by Enrique Rueda on 9/29/18.
+//  Copyright © 2018 appdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
